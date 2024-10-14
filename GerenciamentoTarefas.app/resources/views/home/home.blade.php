cc7f89e227e4eb0a0500c06923be32418c50202b
marcoajr <marcoabjr1994@gmail.com>
2024-10-14 17:27:18 -0300

    Mudaças Pagina Home

--- GerenciamentoTarefas.app/resources/views/home/home.blade.php
+++ GerenciamentoTarefas.app/resources/views/home/home.blade.php
@@ -1,6 +1,90 @@
 @extends('layouts.home.homeLayout')
 
 @section('content')
+<section class="features-icons page-section portfolio" id="portfolio">
+    
+            <div class="container">
+                <!-- Portfolio Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">O Futuro da Organização de Tarefas</h2>
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+            
+                <!-- Portfolio Grid Items-->
+                <div class="row justify-content-center">
+                    <!-- Portfolio Item 1-->
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid" src="{{asset('img/imgBody.jpeg')}}" alt="..." />
+                            <h3>Simplicidade</h3>
+                            <p class="lead mb-0">Simplifique sua rotina com nosso sistema de gerenciamento de tarefas.!</p>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item 2-->
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid" src="{{asset('img/imgBody2.jpeg')}}" alt="..." />
+                            <h3>Transformação</h3>
+                            <p class="lead mb-0">Transforme suas ideias em ações com nossa ferramenta de gestão de tarefas.!</p>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item 3-->
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
+                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid" src="{{asset('img/imgBody3.jpeg')}}" alt="..." />
+                            <h3>Fácil de usar</h3>
+                            <p class="lead mb-0">A maneira mais fácil de gerenciar tarefas.!</p>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+        </section>
+        <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
 
+        <!-- Call to Action-->
+        <section class="call-to-action text-white text-center" id="signup">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <h2 class="mb-4">Comece a usar Hoje mesmo. !!!</h2>
+                        <form class="form-subscribe" id="contactFormFooter" data-sb-form-api-token="API_TOKEN">
+                            
+                            <div class="row">
+                                <div class="col">
+                                    <input class="form-control form-control-lg" id="emailAddressBelow" type="email" placeholder="Email Address" data-sb-validations="required,email" />
+                                    <div class="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:required">Email Address is required.</div>
+                                    <div class="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:email">Endereço de Email não é válido.</div>
+                                </div>
+                                <div class="col-auto"><button class="btn btn-primary btn-lg " id="submitButton" type="submit">Cadastre-se. É Gratis</button></div>
+                            </div>
+            
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    <p>To activate this form, sign up at</p>
+                                    <a class="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 @endsection
--- GerenciamentoTarefas.app/resources/views/layouts/home/homeLayout.blade.php
+++ GerenciamentoTarefas.app/resources/views/layouts/home/homeLayout.blade.php
@@ -20,9 +20,50 @@
         <div class="container">
             @yield('content') 
         </div>
-    
-        <!-- Bootstrap JS Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+        
     </body>
+
+    <!-- Bootstrap core JS-->
+        <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    
+       <!-- Footer-->
+       <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Localização</h4>
+                        <p class="lead mb-0">
+                            Rio de Janeiro - Brasil
+                            <br />
+                            Maricá-Rj, 2490000
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Nossas Redes Sociais </h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><ion-icon name="logo-twitter"></ion-icon></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><ion-icon name="logo-linkedin"></ion-icon></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><ion-icon name="logo-instagram"></ion-icon></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">Trabalhe Conosco.</h4>
+                        <p class="lead mb-0">
+                        Conheca nossa planos de Carreira
+                            <a href="#">Clique Aqui</a>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; 2024 </small></div>
+        </div>
 </html>
